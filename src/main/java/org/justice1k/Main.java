0d26d9806b3ff38f843f5e94fe17163d56c3ea95
justice1k <justiceakpalu11@gmail.com>
2024-11-04 21:26:54 +0000

    bubble

--- src/main/java/org/justice1k/Main.java
+++ src/main/java/org/justice1k/Main.java
@@ -1,4 +1,5 @@
 package org.justice1k;
+import org.justice1k.algorithms.sorting.BubbleSort;
 import org.justice1k.algorithms.sorting.SelectionSort;
 
 import static java.util.Arrays.sort;
@@ -6,8 +7,13 @@ import static java.util.Arrays.sort;
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {2,3,4,3,4,5,5,6,6,9,3};
+        int[] arr = {2,3,4,3,7,4,5,5,6,6,9,3};
 
+
+
+        for (int i : arr){
+            System.out.print(i);
+        }
 
     }
 }
