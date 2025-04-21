e67d5114684036ec670ee5854eb38c08b458e2a4
justice1k <justiceakpalu11@gmail.com>
2025-04-21 16:46:08 +0000

    MergeSort

--- src/main/java/org/justice1k/Main.java
+++ src/main/java/org/justice1k/Main.java
@@ -1,6 +1,7 @@
 package org.justice1k;
 import org.justice1k.algorithms.sorting.BubbleSort;
 import org.justice1k.algorithms.sorting.InsertionSort;
+import org.justice1k.algorithms.sorting.MergeSort;
 import org.justice1k.algorithms.sorting.SelectionSort;
 
 import static java.util.Arrays.sort;
@@ -10,9 +11,14 @@ public class Main {
 
         int[] arr = {2,3,4,3,7,4,5,5,6,6,9,3};
 
-        InsertionSort insertionSort = new InsertionSort();
 
-        insertionSort.sort(arr);
+        MergeSort mergeSort = new MergeSort();
+        int low = 0;
+        int high = arr.length - 1;
+
+        mergeSort.sort(arr,low, high);
+
+
 
         for (int i : arr){
             System.out.print(i);
--- src/main/java/org/justice1k/algorithms/sorting/MergeSort.java
+++ src/main/java/org/justice1k/algorithms/sorting/MergeSort.java
@@ -1,4 +1,50 @@
 package org.justice1k.algorithms.sorting;
 
+import java.util.ArrayList;
+
 public class MergeSort {
+
+    public void sort(int[] arr, int low, int high){
+//        base case
+        if(low >= high) return;
+
+        int mid = (low + high)/ 2;
+//        left part
+        sort(arr, low, mid);
+//        right part
+        sort(arr, mid+1, high);
+        merge(arr, low,mid,high);
+    }
+
+    public void merge(int[] arr, int low, int mid, int high){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int right = mid + 1;
+        int left = low;
+
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else {
+                temp.add(arr[right]);
+                right++;
+            }
+
+        }
+
+        while(left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while(right <= high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i = low; i <= high; i++){
+            arr[i] = temp.get(i - low);
+        }
+
+    }
 }
